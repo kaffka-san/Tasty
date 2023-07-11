cc2fc4bfc6cd5d4c56252416544cf4741f3f7649
Kaffka <kaffka.anastasia@gmail.com>
2023-07-11 15:48:18 +0200

    Initial commit

---
+++ TastyApp.swift
@@ -0,0 +1,17 @@
+//
+//  TastyApp.swift
+//  Tasty
+//
+//  Created by Anastasia Lenina on 11.07.2023.
+//
+
+import SwiftUI
+
+@main
+struct TastyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
